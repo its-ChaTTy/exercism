3c151a25a1f11347fe7a4067bafc331016c565bf
Chaitanya <ct765@snu.edu.in>
2024-04-12 00:10:47 +0530

    isogram

--- practice/isogram/isogram.py
+++ practice/isogram/isogram.py
@@ -1,2 +1,5 @@
+#complete this function to check if the input string is an isogram or not
 def is_isogram(string):
+    scrubbed = string.replace('-', '').replace(' ', '').lower()
+    return len(scrubbed) == len(set(scrubbed))
     pass
