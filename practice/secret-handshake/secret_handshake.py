dd37a3d358be469b5a4d5b692df12b2425e09f2c
Chaitanya <ct765@snu.edu.in>
2024-04-12 12:24:05 +0530

    secret-handshake

--- practice/secret-handshake/secret_handshake.py
+++ practice/secret-handshake/secret_handshake.py
@@ -1,2 +1,9 @@
 def commands(binary_str):
-    pass
+    actions = ['wink', 'double blink', 'close your eyes', 'jump']
+    handshake = []
+    for i in range(4):
+        if binary_str[::-1][i:i+1] == '1':
+            handshake.append(actions[i])
+    if binary_str[0] == '1':
+        handshake.reverse()
+    return handshake
