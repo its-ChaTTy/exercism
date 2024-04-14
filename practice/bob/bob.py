7858ffddb54d9cd995b3a3b979385d17223d2021
Chaitanya <ct765@snu.edu.in>
2024-04-14 22:48:36 +0530

    bob

--- practice/bob/bob.py
+++ practice/bob/bob.py
@@ -1,2 +1,13 @@
 def response(hey_bob):
-    pass
+    hey_bob = hey_bob.rstrip()
+    if hey_bob == '':
+        return "Fine. Be that way!"
+    elif hey_bob.isupper():
+        if hey_bob.endswith('?'):
+            return "Calm down, I know what I'm doing!"
+        else:
+            return "Whoa, chill out!"
+    elif hey_bob.endswith('?'):
+        return "Sure."
+    else:
+        return "Whatever."
