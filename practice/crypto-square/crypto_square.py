8dff4d408201a576363c8bd6c4cb7b6ed148eb4c
Chaitanya <ct765@snu.edu.in>
2024-04-15 09:55:40 +0530

    crypto-square

--- practice/crypto-square/crypto_square.py
+++ practice/crypto-square/crypto_square.py
@@ -1,2 +1,30 @@
+import math
+import re
+
 def cipher_text(plain_text):
-    pass
+    plain_text = re.sub(r'\W+', '', plain_text).lower()
+
+    if not plain_text:
+        return ""
+
+    c = math.ceil(math.sqrt(len(plain_text)))
+    r = len(plain_text) // c
+    if len(plain_text) % c != 0:
+        r += 1
+
+    rows = []
+    for i in range(r):
+        row = plain_text[i*c:(i+1)*c].ljust(c)
+        rows.append(row)
+
+    encoded_text = ""
+    for i in range(c):
+        for row in rows:
+            encoded_text += row[i]
+
+    chunks = []
+    for i in range(c):
+        chunk = encoded_text[i*r:(i+1)*r]
+        chunks.append(chunk)
+
+    return ' '.join(chunks)
