10accbb0614dc3303ae4fef283e10d5b3dcd61ee
Chaitanya <ct765@snu.edu.in>
2024-04-15 19:05:50 +0530

    Pascals Triangle

--- practice/pascals-triangle/pascals_triangle.py
+++ practice/pascals-triangle/pascals_triangle.py
@@ -1,2 +1,13 @@
 def rows(row_count):
-    pass
+    if row_count < 0:
+        raise ValueError("number of rows is negative")
+    elif row_count == 0:
+        return []
+    elif row_count == 1:
+        return [[1]]
+    else:
+        triangle = rows(row_count - 1)
+        last_row = triangle[-1]
+        new_row = [1] + [last_row[i] + last_row[i+1] for i in range(len(last_row) - 1)] + [1]
+        triangle.append(new_row)
+        return triangle
