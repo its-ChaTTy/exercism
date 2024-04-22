6976aa253af0ea7a0c4ae929ee48634622bf570d
Chaitanya <ct765@snu.edu.in>
2024-04-22 14:41:04 +0530

    improved the code

--- practice/raindrops/raindrops.py
+++ practice/raindrops/raindrops.py
@@ -1,13 +1,41 @@
 def convert(number):
-    sounds =[]
-    if number%3==0:
+    """
+    Convert a number into a string that contains raindrop sounds corresponding to certain potential factors.
+
+    A factor is a number that evenly divides into another number, leaving no remainder.
+    The simplest way to test if one number is a factor of another is to use the modulo operation.
+
+    The rules of raindrops are that if a given number:
+
+    - has 3 as a factor, add 'Pling' to the result.
+    - has 5 as a factor, add 'Plang' to the result.
+    - has 7 as a factor, add 'Plong' to the result.
+    - does not have any of 3, 5, or 7 as a factor, the result should be the digits of the number.
+
+    Args:
+        number (int): The number to be converted into a raindrop sound string.
+
+    Returns:
+        str: The resulting raindrop sound string.
+
+    Examples:
+        >>> convert(28)
+        'Plong'
+        >>> convert(30)
+        'PlingPlang'
+        >>> convert(34)
+        '34'
+    """
+    sounds = []
+
+    if number % 3 == 0:
         sounds.append('Pling')
-    if number%5==0:
+    if number % 5 == 0:
         sounds.append('Plang')
-    if number%7==0:
+    if number % 7 == 0:
         sounds.append('Plong')
-    if len(sounds)==0:
+
+    if len(sounds) == 0:
         return str(number)
     else:
         return "".join(sounds)
- 
